bfffc02a57f640afd6d0ee7cbf7328117934052a
Chris Robertson <cjrobertson@worldorderstudios.com>
2019-05-26 11:51:28 -0700

    Initial Project Upload

---
+++ Assets/UDP Example/Scripts/Listener.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Listener : MonoBehaviour
+{
+    public int portNumber = 5000;
+
+    Thread udpListeningThread;
+    Thread udpSendingThread;
+    UdpClient receivingClient;
+
+    void Start()
+    {
+        InitializeListenerThread();
+    }
+
+    private void InitializeListenerThread()
+    {
+        // Create a new thread.
+        udpListeningThread = new Thread(new ThreadStart(UDPListener));
+
+        // Run in background
+        udpListeningThread.IsBackground = true;
+        udpListeningThread.Start();
+
+        Debug.Log("Started on port number: " + portNumber.ToString());
+    }
+
+    public void UDPListener()
+    {
+        receivingClient = new UdpClient(portNumber);
+
+        while (true)
+        {
+            // While Listening 
+            try
+            {
+                IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                // Blocks until a message is returned on this socket from a host.
+                byte[] receiveBytes = receivingClient.Receive(ref remoteIPEndPoint);
+
+                if (receiveBytes != null)
+                {
+                    string returnData = Encoding.ASCII.GetString(receiveBytes);
+                    Debug.Log("Message Received: " + returnData.ToString());
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.Log(exception.ToString());
+            }
+        }
+    }
+}
---
+++ Assets/UDP Example/Scripts/Sender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sender : MonoBehaviour
+{
+    public string messageToSend = "Hello World!";
+
+    private void Start()
+    {
+        // This is here just because I wanted a delay before the script attempted to send a message.
+        Invoke("SendMessage", 3f);
+    }
+
+    public void SendMessage()
+    {
+        MessageSender(messageToSend);
+    }
+
+    static void MessageSender(string message)
+    {
+        Socket socketToUse = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+
+        byte[] sendBuffer = Encoding.ASCII.GetBytes(message);
+        IPEndPoint endPoint = new IPEndPoint(ipAddress, 5000);
+
+        socketToUse.SendTo(sendBuffer, endPoint);
+
+        Debug.Log("Message sent to the required IP address.");
+    }
+}
